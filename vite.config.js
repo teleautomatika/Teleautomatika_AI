d84907151ece1b437186c20fc3c402b6edb5cc61
teleautomatika <git@stackblitz.com>
2024-10-24 14:57:14 -0300

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        login: 'src/login.html',
+        dashboard: 'src/dashboard.html'
+      }
+    }
+  }
+});
